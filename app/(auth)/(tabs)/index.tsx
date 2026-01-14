f6e53d29403791a9277767dcde91c781b1d47d54
bilguun <bilguun@datacloud.mn>
2026-01-14 16:43:37 +0800

    uniwind conf

--- app/(auth)/(tabs)/index.tsx
+++ app/(auth)/(tabs)/index.tsx
@@ -8,8 +8,9 @@ export default function HomeScreen() {
     <View style={{
       paddingTop: 40,
     }}>
-      <Text className="text-xl text-red-400">Home</Text>
+      <Text>Home</Text>
       <Button title="go to login" onPress={() => router.push("/login")} />
+      <Text className="text-xl text-red-400">Red text</Text>
     </View>
   );
 }
